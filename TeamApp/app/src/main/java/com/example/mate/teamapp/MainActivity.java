a9a343075c81e7c36e605288e2b91cecc9597aa9
gregor07 <g.mate778@gmail.com>
2018-12-06 19:30:26 +0100

    newFragment
    newFragment

--- TeamApp/app/src/main/java/com/example/mate/teamapp/MainActivity.java
+++ TeamApp/app/src/main/java/com/example/mate/teamapp/MainActivity.java
@@ -1,21 +1,29 @@
 package com.example.mate.teamapp;
 
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.media.MediaPlayer;
-import android.os.Bundle;
 import android.os.CountDownTimer;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
 import java.util.Locale;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
     MediaPlayer player;
     private static final long START_TIME__IN_MILLISECONDS=30000;
 
+    private Toolbar toolbar;
     private TextView mTextViewCountdown;
     private Button mButtonStartPause;
     private Button mButtonReset;
@@ -30,12 +38,26 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
         //összekötöm a vezérlőt
         mTextViewCountdown = findViewById(R.id.text_view_countdown);
         mButtonStartPause =  findViewById(R.id.button_start_pause);
         mButtonReset =  findViewById(R.id.button_reset);
         timePicker = findViewById(R.id.timePicker);
         timePicker.setIs24HourView(true);
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.setCon
 
         music = MediaPlayer.create(MainActivity.this,R.raw.finish);
 
@@ -49,7 +71,6 @@ public class MainActivity extends Activity {
                 mTextViewCountdown.setText(timeLeftFormatted);
             }
         });
-
         mButtonStartPause.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -67,10 +88,7 @@ public class MainActivity extends Activity {
                 resetTimer();
             }
         });
-
-
     }
-    //
     private  void startTimer(){
         // megadjuk, hogy mikor kéne lejárnia a számlálónak.
         // Így kiküszöbölhető az elforgatásokból adódó idő különbözet
@@ -91,13 +109,12 @@ public class MainActivity extends Activity {
                 updateButtons();
                 mTextViewCountdown.setText("00:00");
             }
-        // amint rákattintunk a start gombra, a gomb meghívja majd ezt a startTimer függvényt és elkezdődik a másodpercek számlálása.
+            // amint rákattintunk a start gombra, a gomb meghívja majd ezt a startTimer függvényt és elkezdődik a másodpercek számlálása.
         }.start();
         //Ha már fut a számlálónk, akkor a felirata ezentúl a Pause felirat legyen
         mTimerRunning = true;
         updateButtons();
     }
-
     //ez a függvény lestoppolja a számlálót
     private void pauseTimer(){
         mCountDownTimer.cancel();
@@ -190,7 +207,6 @@ public class MainActivity extends Activity {
             mCountDownTimer.cancel();
         }
     }
-
     @Override
     protected void onStart() {
         super.onStart();
@@ -215,5 +231,27 @@ public class MainActivity extends Activity {
                 startTimer();
             }
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
